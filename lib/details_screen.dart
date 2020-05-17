695c78e1536443a945b51a8adc9ad251a2199af5
Garrike <43554965+Garrike@users.noreply.github.com>
2020-05-17 05:27:27 -0400

    mudanças na tela de detalhes

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -22,6 +24,8 @@ class _DetailsScreenState extends State<DetailsScreen> {
   ScrollController _controller;
   double _percentColor, _percentOpacity;
   Color _color = Colors.white;
+  int _count = 1;
+  StreamController<int> streamController = StreamController<int>();
 
   @override
   void initState() {
@@ -29,6 +33,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
     _controller = ScrollController();
     _percentColor = .65;
     _percentOpacity = 1;
+    streamController.sink.add(_count);
 
     Future.delayed(Duration(milliseconds: 500), () {
       setState(() {
@@ -73,6 +78,12 @@ class _DetailsScreenState extends State<DetailsScreen> {
         transitionDuration: Duration(milliseconds: 600),
       ));
     return true;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    streamController.close();
   }
 
   @override
@@ -313,46 +324,71 @@ class _DetailsScreenState extends State<DetailsScreen> {
               ]))
             ],
           ),
-          Column(
-            children: <Widget>[
-              Expanded(child: Container()),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(24),
-                      topRight: Radius.circular(24)),
-                  border: Border.all(color: Colors.black26, width: .4),
-                  color: Colors.white,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    GestureDetector(
-                      child: AnimatedContainer(
-                        duration: Duration(microseconds: 200),
-                        curve: Curves.easeIn,
-                        padding:
-                            EdgeInsets.symmetric(vertical: 20, horizontal: 27),
+          // addCart1(context),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              height: 60,
+              child: Row(
+                children: [
+                  Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width * .45,
+                    child: Center(
+                      child: Container(
+                        height: 30,
+                        width: 115,
                         decoration: BoxDecoration(
-                          color: kPrimaryColor.withOpacity(_percentColor),
-                          borderRadius: BorderRadius.circular(27),
+                          border: Border.all(),
+                          borderRadius: BorderRadius.all(Radius.circular(40))
                         ),
                         child: Row(
-                          children: <Widget>[
-                            Text(
-                              "Add carrinho",
-                              style: Theme.of(context).textTheme.button,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.add),
+                              alignment: Alignment.center, 
+                              color: kPrimaryColor,
+                              onPressed: () {
+                                _count++;
+                                streamController.sink.add(_count);
+                              },
                             ),
-                            SizedBox(width: 15),
-                            SvgPicture.asset(
-                              "assets/icons/forward.svg",
-                              height: 11,
+                            StreamBuilder<Object>(
+                              stream: streamController.stream,
+                              initialData: 1,
+                              builder: (context, snapshot) {
+                                return Text(snapshot.data.toString());
+                              }
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.remove),
+                              alignment: Alignment.center, 
+                              color: kPrimaryColor,
+                              onPressed: () {
+                                _count--;
+                                streamController.sink.add(_count);
+                              },
                             ),
                           ],
                         ),
                       ),
-                      onLongPress: () {
+                    ),
+                  ),
+                  Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width * .55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40)
+                      ),
+                      color: kPrimaryColor
+                    ),
+                    child: FlatButton(
+                      onPressed: () {
                         setState(() {
                           _percentColor = 1;
                           carrinho += 1;
@@ -364,108 +400,32 @@ class _DetailsScreenState extends State<DetailsScreen> {
                             cart.add(widget.product);
                           }
                         });
-                        Toast.show(
-                          "Adicionado ao carrinho",
-                          context,
-                          duration: Toast.LENGTH_SHORT,
-                          gravity: Toast.CENTER,
-                        );
                       },
-                      onLongPressEnd: (details) {
-                        setState(() {
-                          _percentColor = .65;
-                        });
-                      },
-                      onTap: () {
-                        setState(() {
-                          _percentColor = 1;
-                          carrinho += 1;
-                          if(cart.contains(widget.product)) {
-                            int index = cart.indexWhere((element) => element == widget.product);
-                            cart[index].quantidade +=  1;
-                          } else {
-                            widget.product.quantidade = 1;
-                            cart.add(widget.product);
-                          }                          
-                        });
-                        Toast.show(
-                          "Adicionado ao carrinho",
-                          context,
-                          duration: Toast.LENGTH_SHORT,
-                          gravity: Toast.CENTER,
-                        );
-                        Future.delayed(Duration(milliseconds: 200), () {
-                          setState(() {
-                            _percentColor = .65;
-                          });
-                        });
-                      },
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          PageRouteBuilder(
-                            pageBuilder: (BuildContext context, _, __) {
-                              return Cart();
-                            },
-                          ),
-                        );
-                        Toast.show(
-                          "Abrir Carrinho",
-                          context,
-                          duration: Toast.LENGTH_SHORT,
-                          gravity: Toast.CENTER,
-                        );
-                      },
-                      child: Container(
-                        height: 70,
-                        width: 70,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: kPrimaryColor.withOpacity(.26),
-                        ),
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: <Widget>[
-                            Container(
-                              padding: EdgeInsets.all(15),
-                              height: 50,
-                              width: 50,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: kPrimaryColor,
-                              ),
-                              child: SvgPicture.asset("assets/icons/bag.svg"),
-                            ),
-                            Positioned(
-                              right: 15,
-                              bottom: 10,
-                              child: Container(
-                                alignment: Alignment.center,
-                                height: 20,
-                                width: 20,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: kWhiteColor,
-                                ),
-                                child: Text(
-                                  carrinho.toString(),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .button
-                                      .copyWith(color: kPrimaryColor, fontSize: 16),
-                                ),
-                              ),
-                            )
-                          ],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40)
                         ),
                       ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 30,
+                            width: 30,
+                            child: SvgPicture.asset("assets/icons/bag.svg"),
+                          ),
+                          SizedBox(width: 10,),
+                          Text(
+                            'Add Carrinho',
+                            style: Theme.of(context).textTheme.headline6.copyWith(fontSize: 16)
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
+                  )
+                ],
               ),
-            ],
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 20.0),
@@ -508,5 +468,162 @@ class _DetailsScreenState extends State<DetailsScreen> {
         ],
       )),
     );
+  }
+
+  Column addCart1(BuildContext context) {
+    return Column(
+          children: <Widget>[
+            Expanded(child: Container()),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(24)),
+                border: Border.all(color: Colors.black26, width: .4),
+                color: Colors.white,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  GestureDetector(
+                    child: AnimatedContainer(
+                      duration: Duration(microseconds: 200),
+                      curve: Curves.easeIn,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 27),
+                      decoration: BoxDecoration(
+                        color: kPrimaryColor.withOpacity(_percentColor),
+                        borderRadius: BorderRadius.circular(27),
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            "Add carrinho",
+                            style: Theme.of(context).textTheme.button,
+                          ),
+                          SizedBox(width: 15),
+                          SvgPicture.asset(
+                            "assets/icons/forward.svg",
+                            height: 11,
+                          ),
+                        ],
+                      ),
+                    ),
+                    onLongPress: () {
+                      setState(() {
+                        _percentColor = 1;
+                        carrinho += 1;
+                        if(cart.contains(widget.product)) {
+                          int index = cart.indexWhere((element) => element == widget.product);
+                          cart[index].quantidade +=  1;
+                        } else {
+                          widget.product.quantidade = 1;
+                          cart.add(widget.product);
+                        }
+                      });
+                      Toast.show(
+                        "Adicionado ao carrinho",
+                        context,
+                        duration: Toast.LENGTH_SHORT,
+                        gravity: Toast.CENTER,
+                      );
+                    },
+                    onLongPressEnd: (details) {
+                      setState(() {
+                        _percentColor = .65;
+                      });
+                    },
+                    onTap: () {
+                      setState(() {
+                        _percentColor = 1;
+                        carrinho += 1;
+                        if(cart.contains(widget.product)) {
+                          int index = cart.indexWhere((element) => element == widget.product);
+                          cart[index].quantidade +=  1;
+                        } else {
+                          widget.product.quantidade = 1;
+                          cart.add(widget.product);
+                        }                          
+                      });
+                      Toast.show(
+                        "Adicionado ao carrinho",
+                        context,
+                        duration: Toast.LENGTH_SHORT,
+                        gravity: Toast.CENTER,
+                      );
+                      Future.delayed(Duration(milliseconds: 200), () {
+                        setState(() {
+                          _percentColor = .65;
+                        });
+                      });
+                    },
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (BuildContext context, _, __) {
+                            return Cart();
+                          },
+                        ),
+                      );
+                      Toast.show(
+                        "Abrir Carrinho",
+                        context,
+                        duration: Toast.LENGTH_SHORT,
+                        gravity: Toast.CENTER,
+                      );
+                    },
+                    child: Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: kPrimaryColor.withOpacity(.26),
+                      ),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(15),
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: kPrimaryColor,
+                            ),
+                            child: SvgPicture.asset("assets/icons/bag.svg"),
+                          ),
+                          Positioned(
+                            right: 15,
+                            bottom: 10,
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: 20,
+                              width: 20,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: kWhiteColor,
+                              ),
+                              child: Text(
+                                carrinho.toString(),
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .button
+                                    .copyWith(color: kPrimaryColor, fontSize: 16),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
   }
 }
