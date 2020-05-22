cb9699572f393419c3cee31f66d00e97f9b0c5a9
Garrike <43554965+Garrike@users.noreply.github.com>
2020-05-21 20:24:55 -0400

    cart finalizado

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:food_app/details_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:food_app/constants.dart';
@@ -10,6 +10,7 @@ import 'main.dart';
 
 List<Pedido> pedidos;
 double total = 0;
+bool initialSet;
 
 class CartScreen extends StatefulWidget {
   @override
@@ -19,10 +20,12 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   StreamController<List<Pedido>> _streamControllerPedido;
   ScrollController scrollController = ScrollController();
+  SlidableController slidableController = SlidableController();
 
   @override
   void initState() {
     super.initState();
+    initialSet = true;
     pedidos = [];
     total = 0;
     _carregarProdutos();
@@ -78,11 +81,12 @@ class _CartScreenState extends State<CartScreen> {
                   builder: (context, snapshot) {
                     if(snapshot.hasError || !snapshot.hasData) return Center(child: CircularProgressIndicator(),);
                     List<Pedido> cestas = snapshot.data;
-                    cestas.forEach((element) {
-                      element.produtos.forEach((produto) {
-                        total += produto.price * produto.quantidade;
-                      });
-                    });
+                    // total = 0;
+                    // cestas.forEach((element) {
+                    //   element.produtos.forEach((produto) {
+                    //     total = total + produto.price * produto.quantidade;
+                    //   });
+                    // });
                     if(cestas.length == 0) {
                       return Center(
                         child: Text(
@@ -127,41 +131,55 @@ class _CartScreenState extends State<CartScreen> {
                                       itemCount: item.produtos.length,
                                       itemBuilder: (context, index2) {
                                         return Slidable(
+                                          key: Key(item.produtos[index2].title),
+                                          controller: slidableController,
                                           child: Padding(
                                             padding: const EdgeInsets.symmetric(horizontal: 20.0),
                                             child: Row(
                                               children: [
-                                                Container(
-                                                  width: 100,
-                                                  height: 110,
-                                                  child: Stack(
-                                                    children: [
-                                                      Positioned(
-                                                        bottom: 10,
-                                                        left: 0,
-                                                        child: Container(
-                                                          width: 90,
-                                                          height: 90,                                        
-                                                          decoration: BoxDecoration(
-                                                            color: item.produtos[index2].color.withOpacity(.4),
-                                                            borderRadius: BorderRadius.all(Radius.circular(8)),
-                                                          ),
-                                                        ), 
-                                                      ),
-                                                      Positioned(
-                                                        top: 0,
-                                                        right: 0,
-                                                        child: ClipRRect(
-                                                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                                                          child: Image( 
-                                                            height: 90,
-                                                            width: 90,                                         
-                                                            image: AssetImage(item.produtos[index2].image),
-                                                            fit: BoxFit.contain,
+                                                GestureDetector(
+                                                  onTap: () {
+                                                    Navigator.push(
+                                                      context,
+                                                      PageRouteBuilder(
+                                                        pageBuilder: (BuildContext context, _, __) {
+                                                          return DetailsScreen(item.produtos[index2]);
+                                                        },
+                                                      )
+                                                    );
+                                                  },
+                                                  child: Container(
+                                                    width: 100,
+                                                    height: 110,
+                                                    child: Stack(
+                                                      children: [
+                                                        Positioned(
+                                                          bottom: 10,
+                                                          left: 0,
+                                                          child: Container(
+                                                            width: 90,
+                                                            height: 90,                                        
+                                                            decoration: BoxDecoration(
+                                                              color: item.produtos[index2].color.withOpacity(.4),
+                                                              borderRadius: BorderRadius.all(Radius.circular(8)),
+                                                            ),
+                                                          ), 
+                                                        ),
+                                                        Positioned(
+                                                          top: 0,
+                                                          right: 0,
+                                                          child: ClipRRect(
+                                                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                                                            child: Image( 
+                                                              height: 90,
+                                                              width: 90,                                         
+                                                              image: AssetImage(item.produtos[index2].image),
+                                                              fit: BoxFit.contain,
+                                                            ),
                                                           ),
                                                         ),
-                                                      ),
-                                                    ],
+                                                      ],
+                                                    ),
                                                   ),
                                                 ),
                                                 Expanded(
@@ -203,7 +221,8 @@ class _CartScreenState extends State<CartScreen> {
                                                                   if(item.produtos[index2].quantidade > 1) {
                                                                     setState(() {
                                                                       item.produtos[index2].quantidade--;
-                                                                      total -= item.produtos[index2].price; 
+                                                                      total -= item.produtos[index2].price;
+                                                                      carrinho -= 1;
                                                                     });
                                                                   }                                
                                                                 },
@@ -227,7 +246,8 @@ class _CartScreenState extends State<CartScreen> {
                                                                 onTap: () {
                                                                   setState(() {
                                                                     item.produtos[index2].quantidade++;
-                                                                    total += item.produtos[index2].price;                                                       
+                                                                    total = total + item.produtos[index2].price;
+                                                                    carrinho += 1;                                                 
                                                                   });                          
                                                                 },
                                                               ),
@@ -246,8 +266,8 @@ class _CartScreenState extends State<CartScreen> {
                                                       Align(
                                                         alignment: Alignment.centerRight,
                                                         child: Text(
-                                                          '*Valor ajustados',
-                                                          style: Theme.of(context).textTheme.button.copyWith(fontSize: 8),
+                                                          '*Valor ajustado a qtd',
+                                                          style: Theme.of(context).textTheme.button.copyWith(fontSize: 7),
                                                         ),
                                                       ),
                                                       Text(
@@ -263,10 +283,22 @@ class _CartScreenState extends State<CartScreen> {
                                           actionPane: SlidableDrawerActionPane(),
                                           secondaryActions: <Widget>[
                                           IconSlideAction(
-                                            // caption: 'Delete',
                                             color: Colors.red,
                                             icon: Icons.delete,
-                                            onTap: () {},
+                                            onTap: () {
+                                              setState(() {
+                                                carrinho -= item.produtos[index2].quantidade.toInt();
+                                                total = total - item.produtos[index2].price * item.produtos[index2].quantidade;
+                                                cart.remove(item.produtos[index2]);
+                                                cestas.remove(item.produtos[index2]);
+                                                item.produtos.remove(item.produtos[index2]);               
+                                              });
+                                              if (item.produtos.length == 0) {
+                                                setState(() {
+                                                  cestas.remove(item);                                           
+                                                });
+                                              }
+                                            },
                                           ),
                                         ],
                                         );
@@ -398,37 +430,38 @@ class _CartScreenState extends State<CartScreen> {
 }
 
 Future<List<Pedido>> obterPedidos() async {
-    bool verificado = false;
-    PaletteGenerator paletteGenerator;
-    pedidos = [];
-    cart.forEach((element) async {
-      print(element.title);
-      total += element.price * element.quantidade;
-      if(element.color == null) {
-        paletteGenerator = await PaletteGenerator.fromImageProvider(AssetImage(element.image));
-        element.color = paletteGenerator.darkVibrantColor?.color == null ? Colors.grey : paletteGenerator.darkVibrantColor?.color;
-      }      
-      verificado = false;
-      for (var pedido in pedidos) {
-        if(pedido.produtor == element.produtor) {
-          pedido.produtos.add(element);
-          verificado = true;
-          break;
-        }
+  bool verificado = false;
+  PaletteGenerator paletteGenerator;
+  pedidos = [];
+  cart.forEach((element) async {
+    print(element.title);
+    total = total + element.price * element.quantidade;
+    if(element.color == null) {
+      paletteGenerator = await PaletteGenerator.fromImageProvider(AssetImage(element.image));
+      element.color = paletteGenerator.darkVibrantColor?.color == null ? Colors.grey : paletteGenerator.darkVibrantColor?.color;
+    }      
+    verificado = false;
+    for (var pedido in pedidos) {
+      if(pedido.produtor == element.produtor) {
+        pedido.produtos.add(element);
+        verificado = true;
+        break;
       }
-      if(!verificado) {
-        pedidos.add(Pedido(
-        produtor: element.produtor,
-        produtos: [element],
-        ));
-      }
-    });
-    print('Pedidos: ${pedidos.length}');
-    pedidos.forEach((element) {print(element.produtos.length);});
-    
-    await Future.delayed(Duration(seconds: 2));
-    return pedidos;
-  }
+    }
+    if(!verificado) {
+      pedidos.add(Pedido(
+      produtor: element.produtor,
+      produtos: [element],
+      ));
+    }
+  });
+  initialSet = false;
+  print('Pedidos: ${pedidos.length}');
+  pedidos.forEach((element) {print(element.produtos.length);});
+  
+  await Future.delayed(Duration(seconds: 2));
+  return pedidos;
+}
 
 class Pedido {
   final String produtor;
